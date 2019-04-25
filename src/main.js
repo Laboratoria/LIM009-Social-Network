8dc200ce658500949117a69992cf0c798fcb82df
marjo1994 <m_rhl@live.com>
2019-04-24 17:05:50 -1100

    Cambios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
+const btnSubmit = document.getElementById('btn-submit');
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+btnSubmit.addEventListener('click', () => {
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then(result => {
+    console.log(result.user);
+    btnSubmit.append('src=""')
+  })
+});
+
+
+
+
+
+
+// btnSubmit.addEventListener('click',()=>{
+//   firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+//    .catch(function(error) {
+//        // Handle Errors here.
+//        var errorCode = error.code;
+//        var errorMessage = error.message;
+//        // ...
+//      });
+// })
