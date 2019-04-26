54669e9dfa190eb967fc5dd32aa6b698b149156f
marjo1994 <m_rhl@live.com>
2019-04-25 13:03:49 -1100

    Inicio de sesión con Google

--- src/main.js
+++ src/main.js
@@ -7,24 +7,14 @@
 // const email = document.getElementById('email');
 // const password = document.getElementById('password');
 const btnSubmit = document.getElementById('btn-submit');
-
+const btnGoogle = document.getElementById('btn-google');
+const root = document.getElementById('root');
 var provider = new firebase.auth.GoogleAuthProvider();
-
-btnSubmit.addEventListener('click', () => {
-  firebase.auth()
-  .signInWithPopup(provider)
-  .then(result => {
-    console.log(result.user);
-    btnSubmit.append('src=""')
-  })
-});
-
-
-
-
 
 
 // btnSubmit.addEventListener('click',()=>{
+//   console.log(email.value);
+//   console.log(password.value);
 //   firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
 //    .catch(function(error) {
 //        // Handle Errors here.
@@ -33,3 +23,31 @@ btnSubmit.addEventListener('click', () => {
 //        // ...
 //      });
 // })
+
+btnGoogle.addEventListener('click', () => {
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then(result => {
+    console.log(result.user);
+    guardarDatosFirebase(result.user);
+    root.innerHTML=`<img src='${result.user.photoURL}'/>`;
+  })
+});
+
+const guardarDatosFirebase = user => {
+  const usuario = {
+    uid : user.uid,
+    nombre : user.displayName,
+    email : user.email,
+    foto : user.photoURL
+  }
+  firebase.database().ref(`colección/${user.uid}`).set(usuario);
+}
+
+firebase.database().ref('colección')
+.on('child_added', s => {
+  var user = s.val();
+  root.innerHTML=`<img width ='100px' src='${user.foto}'/>`;
+})
+
+
