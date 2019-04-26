cb3726ea581ddec0662190f3c078e1ad6651fdc4
marjo1994 <m_rhl@live.com>
2019-04-26 01:03:12 -0500

    Inicio de sesión desde una cuenta nueva

--- src/main.js
+++ src/main.js
@@ -4,25 +4,39 @@
 
 // myFunction();
 
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const email2 = document.getElementById('email2');
+const password2 = document.getElementById('password2');
+const btnRegister = document.getElementById('btn-register');
 const btnSubmit = document.getElementById('btn-submit');
 const btnGoogle = document.getElementById('btn-google');
 const root = document.getElementById('root');
 var provider = new firebase.auth.GoogleAuthProvider();
 
 
-// btnSubmit.addEventListener('click',()=>{
-//   console.log(email.value);
-//   console.log(password.value);
-//   firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-//    .catch(function(error) {
-//        // Handle Errors here.
-//        var errorCode = error.code;
-//        var errorMessage = error.message;
-//        // ...
-//      });
-// })
+btnRegister.addEventListener('click',()=>{
+  firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+   .catch(function(error) {
+       // Handle Errors here.
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       console.log(errorCode);
+       console.log(errorMessage);
+     });
+})
+
+btnSubmit.addEventListener('click',()=>{
+  firebase.auth().signInWithEmailAndPassword(email2.value, password2.value).
+  catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
+})
+
 
 btnGoogle.addEventListener('click', () => {
   firebase.auth()
