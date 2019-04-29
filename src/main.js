7e91f763db5e00b098c6945fbce4a997b6fa06e7
marjo1994 <m_rhl@live.com>
2019-04-29 11:57:23 -1100

    Utilizando SPA

--- src/main.js
+++ src/main.js
@@ -1,19 +1,48 @@
 // Este es el punto de entrada de tu aplicacion
 
 import SignInForm from './view/SignInForm.js';
+import Utils from './services/Utils.js';
 
-import {loginGoogle}  from './lib/index.js';
+// import {loginGoogle}  from './lib/index.js';
+
+// const btnGoogle = document.getElementById('btn-google');
+
+// btnGoogle.addEventListener('click', loginGoogle);
 
 
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const email2 = document.getElementById('email2');
-const password2 = document.getElementById('password2');
-const btnRegister = document.getElementById('btn-register');
-const btnSubmit = document.getElementById('btn-submit');
-const btnGoogle = document.getElementById('btn-google');
-const root = document.getElementById('root');
+const keyFirebase = () => {
+    var config = {
+        apiKey: "AIzaSyCkwXG7Zx6u0mEUPjzTL19bG6O8bLQSyZY",
+        authDomain: "red-social-e9df5.firebaseapp.com",
+        databaseURL: "https://red-social-e9df5.firebaseio.com",
+        projectId: "red-social-e9df5",
+        storageBucket: "red-social-e9df5.appspot.com",
+        messagingSenderId: "258184809756"
+      };
+      firebase.initializeApp(config);
+}
 
-SignInForm.signInForm();
+keyFirebase();
 
-btnGoogle.addEventListener('click', loginGoogle);
+const routes = {
+    '/': SignInForm
+    // , '/register': SignUpForm
+    // , '/login': login
+    // , '/post': post
+  };
+  
+   export const router = async() => {
+        const content = null || document.getElementById('page_container');
+        content.innerHTML = await SignInForm.render();
+        await SignInForm.after_render();
+
+        let request = Utils.parseRequestURL();
+        let currentURL = (request.resource ? '/' + request.resource : '/');
+        let currentPage = routes[currentURL] ? routes[currentURL] : Error404;
+        content.innerHTML = await currentPage.render();
+        await currentPage.after_render();
+
+  };
+  
+  window.addEventListener('hashchange', router);
+  window.addEventListener('load', router);
--- src/view/SignInForm.js
+++ src/view/SignInForm.js
@@ -1,40 +1,25 @@
 // form de inicio de sesión
 let SignInForm = {
-
-    signInForm() {
-
-        const form = document.createElement('form');
-    // input email
-        const email = document.createElement('input');
-        email.type='email';
-        // email.classList.add('espacio');
-        email.placeholder ='Email';
-    // input password 
-        const password = document.createElement('input');
-        password.type ='password';
-        // password.classList.add('espacio');
-        password.placeholder = 'Password'
-    
-    // boton para iniciar sesion
-        const btnLogin  = document.createElement('button');
-        btnLogin.innerHTML ='Log in';
-        // btnLogin.classList.add('espacio');
-    
-        form.appendChild(email)
-        document.getElementById('root').appendChild(form)
-        this.insertAfter(email,password)
-        this.insertAfter(password,btnLogin)
+    render : async () => {
+        let view = `
+            <section>
+                <form>
+                    <h3>Crianza Respetuosa</h3>
+                    <input type="email" id="email2" placeholder="Escriba su email">
+                    <input type="password" id="password2" placeholder="Escriba su contraseña">
+                    <button id="btn-submit">Log in</button>
+                    <button id="btn-google">Google</button>
+                    <button id="btn-fb">Google</button>
+                </form>
+            </section>
+            `;
+        return view;
     },
-
-        insertAfter(current,next) { 
-        if(current.nextSibling){ 
-            current.parentNode.insertBefore(next,current.nextSibling); 
-        } else { 
-            current.parentNode.appendChild(next); 
-        }
-    }
-}
+    after_render :  async () => {}
+ }
 
 
 export default SignInForm;
+
+
 
