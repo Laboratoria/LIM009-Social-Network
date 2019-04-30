5a8021a8d69bd504986eb23e7e10000aa7760ad9
susanita12352 <scahuantico@gmail.com>
2019-04-30 11:27:36 -0500

    logueando mi usuario

--- src/main.js
+++ src/main.js
@@ -2,4 +2,32 @@
 
 import { myFunction } from './lib/index.js';
 
+const registrar = () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('contraseña').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  }
+
+const ingreso = () => {
+    const email2 = document.getElementById('email2').value;
+    const password2 = document.getElementById('contraseña2').value;
+
+    firebase.auth().signInWithEmailAndPassword(email2, password2)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // ...
+    }); 
+  }
+
 myFunction();
