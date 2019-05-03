43c4725515ee0bb66152fb5813d9cd9bbcf87c1a
MAYTESI <mayte16inga@gmail.com>
2019-05-03 18:42:48 -0500

    subiendo cambios de errores

--- src/controller/controller1.js
+++ src/controller/controller1.js
@@ -3,14 +3,51 @@ import { signIn, signUp, signInWithGoogle, signInWithFacebook } from "../service
 const signInOnSubmit = () => {
   const email = document.querySelector('#email').value;
   const password = document.querySelector('#password').value;
-  return signIn(email, password);
+  return signIn(email, password)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      if (errorCode == 'auth/weak-password') {
+        alert('El nivel de seguridad de la contraseña es : débil.');
+      } else  if (errorCode =="auth/email-already-in-use"){
+        alert ('Ya existe esta cuenta')
+      } else if (errorCode =='auth/invalid-email'){
+        alert ('La dirección de correo electrónico es inválida')}
+        else if (errorCode =='auth/invalid-email'){
+          alert ('La dirección de correo electrónico es inválida')}
+          else { alert('No hay registro de usuario correspondiente a este identificador. El usuario puede haber sido eliminado.')
+          }      
+      console.log(error);
+    });
 };
 
 
 const signUpOnSubmit = () => {
   const email2 = document.querySelector('#email2').value;
   const password2 = document.querySelector('#password2').value;
-  return signUp(email2, password2);
+  return signUp(email2, password2)
+  .then((result) => {
+    console.log(" exitosamente registrado");
+  })
+  .catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage);
+    if (errorCode == 'auth/weak-password') {
+      alert('El nivel de seguridad de la contraseña es : débil.');
+    } else  if (errorCode =="auth/email-already-in-use"){
+      alert ('Ya existe esta cuenta')
+    } else if (errorCode =='auth/invalid-email'){
+      alert ('La dirección de correo electrónico es inválida')}
+      else if (errorCode =='auth/invalid-email'){
+        alert ('La dirección de correo electrónico es inválida')}
+        else { alert('No hay registro de usuario correspondiente a este identificador. El usuario puede haber sido eliminado.')
+        }      
+    console.log(error);
+  });
 };
 
 const signInOnSubmitGoogle = () => {
@@ -18,7 +55,15 @@ const signInOnSubmitGoogle = () => {
     .then(result => {
       console.log("exitosamente con google");
     })
-    .catch(error => {
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
       console.log(error);
     });
 }
@@ -28,9 +73,18 @@ const signInOnSubmitFacebook = () => {
     .then((result) => {
       console.log(" exitosamente con facebook");
     })
-    .catch((error) => {
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
       console.log(error);
     });
 }
+
 
 export { signInOnSubmit, signUpOnSubmit, signInOnSubmitGoogle, signInOnSubmitFacebook };
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,22 +2,19 @@
 const signUp = (email, password) => {
   return firebase.auth().createUserWithEmailAndPassword(email, password);
 }
-export { signUp };
 
 // Inicion de sesión  con solo email y contraseña
 const signIn = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);
-export { signIn }
 
 // Inicio de sesión con g-mail y contraseña de g-mail
 const signInWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   return firebase.auth().signInWithPopup(googleProvider);
 };
-export { signInWithGoogle }
 
 // Inicio de sesión con  cuenta de facebook y contraseña de facebook
 const signInWithFacebook = () => {
   const facebookProvider = new firebase.auth.FacebookAuthProvider();
   return firebase.auth().signInWithPopup(facebookProvider);
 };
-export { signInWithFacebook };
+export { signUp, signIn, signInWithGoogle, signInWithFacebook };
