b100c03c2207563dbe57a7e43a32814caeef033d
maytezhou <maytezhou14@gmail.com>
2019-05-04 11:56:48 -0500

    cambio  de vistas de login a profile user   y de profile user a inicio de sesion

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,37 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from "../src/lib/index";
+import { signUpOnSubmit } from "../src/controller/controller1.js";
+import { signInOnSubmit} from '../src/controller/controller1.js'
+  
+describe('signInOnSubmit', () => {
+  it('Deberia poder iniciar sesión  con email: test@test.la y password:abc123') // asercion 
+  //test con jest */
+  addNote()
+});
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
+describe('signUpOnSubmit', () => {
+  it('debería poder registrarme', () => {
     expect(typeof myFunction).toBe('function');
   });
 });
+
+//Creando mi firebase falso 
+//
+const auth = () => {
+  return {
+      signInWithEmailAndPassword: (emailLogIn, passwordLogIn) => {
+          return new Promise((resolve) => {
+              resolve({
+                  email: emailLogIn,
+              })
+          })
+      }
+  }
+};
+
+const firebase = {
+  auth: auth,
+  // initializeApp() {}
+};
+export default jest.fn(() => {
+  return firebase
+}) 
