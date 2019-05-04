767d2950395b4d2ad550213fcb92b761a95b058c
nayruthCalla <nayruth.calla@gmail.com>
2019-05-03 19:26:03 -0500

    agregando diseño a la vista registro usuario #12

--- src/view/pagesRegister.js
+++ src/view/pagesRegister.js
@@ -1,26 +1,27 @@
-import  {registerUser}  from '../controller/view-controller.js'
+import { registerUser } from '../controller/view-controller.js'
 
 export default () => {
-const formRegister = document.createElement('form');
-const templateRegister = `<div><input type="email" id="email-register" />
-<input type="password" id="password-register" />
-<button type="button" id="btn-register">Registrarse</button>
-</div>`;
-formRegister.innerHTML = templateRegister;
-const btnRegister = formRegister.querySelector('#btn-register');
-// const btnRegister = formRegister.querySelector('#btn-register');
-const emailRegister = formRegister.querySelector('#email-register');
-const passwordRegister = formRegister.querySelector('#password-register');
+    const formRegister = document.createElement('div');
+    const templateRegister = `
+    <div class= 'col-12 col-xs-12'>
+    <div class= 'col-xs-12 center style-border'>
+    <header><img src="./image/logo.png" alt="educacion" class='block center'>
+       <h2 class='center'>  << Por una educación respetuosa >>  </h2>
+    </header>
+      <form class = 'center'><input type="email" id="email-register" class='style-input center block border' placeholder ='Escriba su email'/>
+       <input type="password" id="password-register" class='style-input center block border' placeholder ='Escriba su contraseña' />
+       <p id = 'mjs-error-register' class = 'center block' ></p>
+       <button type="button" id="btn-register" class='center style-btn-login block border' >Registrarse</button>
+          </form>
+       <footer>
+         <a href='#/login' class= 'style-register'>Regresar</a></span>
+       <footer>
+   </div>
+   </div>
+   `;
+    formRegister.innerHTML = templateRegister;
 
-btnRegister.addEventListener('click',()=>{
-    // console.log(emailRegister.value);
-    // console.log(passwordRegister.value)
-    registerUser(emailRegister.value,passwordRegister.value)
-})
-// btnRegister.addEventlistener('click', ()=>{
-//     console.log('holaaaaaaa')
-// });
-console.log(formRegister)
-return formRegister;
-
+    const btnRegister = formRegister.querySelector('#btn-register');
+    btnRegister.addEventListener('click', registerUser)
+    return formRegister;
 };
