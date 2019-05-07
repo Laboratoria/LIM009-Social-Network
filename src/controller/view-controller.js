8b54c9236f79e4f10483b2b9aa35258fc3f8fa6f
nayruthCalla <nayruth.calla@gmail.com>
2019-05-06 22:51:16 -0500

    resolviendo conflictos

--- src/controller/view-controller.js
+++ src/controller/view-controller.js
@@ -1,4 +1,4 @@
-import { signInWithEmail, signInWithGoogle, signInWithFacebook ,createEmailAndPassword} from "../lib/lib-firebase.js";
+import { signInWithEmail, signInWithGoogle, signInWithFacebook, createEmailAndPassword } from "../lib/lib-firebase.js";
 import { dataBaseUser } from '../model/model.js'
 
 const changeHash = (hash) => {
@@ -6,67 +6,68 @@ const changeHash = (hash) => {
 }
 
 export const registerUser = () => {
-const emailRegister = document.querySelector('#email-register').value;
-const passwordRegister = document.querySelector('#password-register').value;
-return  createEmailAndPassword(emailRegister,passwordRegister)
-  .then((result)=> {
-    dataBaseUserCreateEmail(result.user)}
-  ).catch(error => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    if(errorCode == 'auth/invalid-email') {
-      alert('El correo es inválido');
-    } else if(errorCode == 'auth/email-already-in-use') {
-      alert('El correo ya ha sido utilizado');
-    } else if(errorCode == 'auth/weak-password') {
-      alert('La contraseña no es lo suficientemente fuerte')
-    } else {
-      alert(errorMessage);
+  const emailRegister = document.querySelector('#email-register').value;
+  const passwordRegister = document.querySelector('#password-register').value;
+  return createEmailAndPassword(emailRegister, passwordRegister)
+    .then((result) => {
+      dataBaseUserCreateEmail(result.user)
     }
-  });
+    ).catch(error => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/invalid-email') {
+        alert('El correo es inválido');
+      } else if (errorCode == 'auth/email-already-in-use') {
+        alert('El correo ya ha sido utilizado');
+      } else if (errorCode == 'auth/weak-password') {
+        alert('La contraseña no es lo suficientemente fuerte')
+      } else {
+        alert(errorMessage);
+      }
+    });
 }
 
 export const email = () => {
-const valueEmail = document.querySelector("#email-id").value;
-const password = document.querySelector("#password-id").value;
+  const valueEmail = document.querySelector("#email-id").value;
+  const password = document.querySelector("#password-id").value;
   return signInWithEmail(valueEmail, password)
     .then((result) => {
       console.log(result);
     }).catch(error => {
-    // Handle Errors here.
+      // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       if (errorCode == 'auth/user-not-found') {
-      alert('El correo no ha sido registrado');
+        alert('El correo no ha sido registrado');
       } else if (errorCode == 'auth/invalid-email') {
-      alert('El correo es inválido')
-      } else if(errorCode == 'auth/wrong-password') {
-      alert('La contraseña es equivocada.')
+        alert('El correo es inválido')
+      } else if (errorCode == 'auth/wrong-password') {
+        alert('La contraseña es equivocada.')
       } else {
-      alert(errorMessage);
+        alert(errorMessage);
       }
     });
 };
 
 export const google = () => {
   return signInWithGoogle().then(result => {
-    dataBaseUser(result.user)  
-    }).catch(error => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      if (errorCode == 'auth/cancelled-popup-request') {
-        alert('Sólo se permite una solicitud emergente a la vez.');
-      } else if (errorCode == 'auth/invalid-email') {
-        alert('El correo es inválido')
-      } else if(errorCode == 'auth/wrong-password') {
-        alert('La contraseña es equivocada.')
-      } else if(errorCode == 'auth/account-exists-with-different-credential') {
-        alert('La cuenta ya ha sido utilizada con una credencial diferente')
-      } else {      
-        alert(errorMessage);
-      }     
-    });
+    dataBaseUser(result.user)
+  }).catch(error => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/cancelled-popup-request') {
+      alert('Sólo se permite una solicitud emergente a la vez.');
+    } else if (errorCode == 'auth/invalid-email') {
+      alert('El correo es inválido')
+    } else if (errorCode == 'auth/wrong-password') {
+      alert('La contraseña es equivocada.')
+    } else if (errorCode == 'auth/account-exists-with-different-credential') {
+      alert('La cuenta ya ha sido utilizada con una credencial diferente')
+    } else {
+      alert(errorMessage);
+    }
+  });
 }
 
 export const facebook = () => {
@@ -79,13 +80,13 @@ export const facebook = () => {
       alert('Sólo se permite una solicitud emergente a la vez.');
     } else if (errorCode == 'auth/invalid-email') {
       alert('El correo es inválido')
-    } else if(errorCode == 'auth/wrong-password') {
+    } else if (errorCode == 'auth/wrong-password') {
       alert('La contraseña es equivocada.')
-    } else if(errorCode == 'auth/account-exists-with-different-credential') {
+    } else if (errorCode == 'auth/account-exists-with-different-credential') {
       alert('La cuenta ya ha sido utilizada con una credencial diferente')
-    } else {      
+    } else {
       alert(errorMessage);
-    }     
+    }
   });
 }
 
