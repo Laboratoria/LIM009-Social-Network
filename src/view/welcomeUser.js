1a89c8d2230e2f65f55399100c29fcd166b2fcf8
nayruthCalla <nayruth.calla@gmail.com>
2019-05-14 22:25:07 -0500

    pagina home responsive closed #21

--- src/view/welcomeUser.js
+++ src/view/welcomeUser.js
@@ -4,6 +4,7 @@ import { logOut, createPost } from '../controller/view-controller.js';
 export default (user) => {
     const root = document.getElementById('root')
     const templateWelcome = `
+    <div class = 'col-12'>
     <header>
         <ul class = 'header'>
             <li><a id = 'edit-perfil' href='#/edit-perfil'>${user.displayName}</a></li>
@@ -11,6 +12,7 @@ export default (user) => {
             <li><a id='sign-out'>Cerrar Sesión</a></li>
         </ul>
     </header>
+    <div class = 'col-4'>              
     <div>
     <p>Nombre del usuario</p>
     <span id='user-name'>${user.displayName}</span>
@@ -19,13 +21,18 @@ export default (user) => {
     <p>Foto</p>
     <img src='${user.photoURL}'>
     </div>
+    </div>
+    <div id = 'post' class='col-7 col-xs-12 center'>
     <form id= 'create-post'>
         <label>Crear una publicación</label>
         <input type = 'text' id = 'description' placeholder='¿Qué estás pensando?'></input>
         <button type = 'button' id ='btn-share'>Compartir</button>
     </form>
+    
     <li id='post-list'>
     </li>
+    </div>
+    </div>
     `;
     root.innerHTML = templateWelcome;
     const btnSignOut = document.querySelector('#sign-out');
