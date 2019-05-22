f40b5c4ccf6272f249078790a0278fe28b43d5ed
MAYTESI <mayte16inga@gmail.com>
2019-05-21 23:59:43 -0500

    cambios para unir a upstream master

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,72 +1,4 @@
-// Registro con solo correo y contraseña
-const signUp = (email, password) => {
-    return firebase.auth().createUserWithEmailAndPassword(email, password);
-}
 
-// Inicion de sesión  con solo email y contraseña
-const signIn = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);
-
-// Inicio de sesión con g-mail y contraseña de g-mail
-const signInWithGoogle = () => {
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
-    return firebase.auth().signInWithPopup(googleProvider);
-
-};
-
-// Inicio de sesión con  cuenta de facebook y contraseña de facebook
-const signInWithFacebook = () => {
-    const facebookProvider = new firebase.auth.FacebookAuthProvider();
-    return firebase.auth().signInWithPopup(facebookProvider);
-};
-
-// Cerrar seión
-const signOut = () => {
-    return firebase.auth().signOut();
-};
-const dataBaseCloudFirestore = () => {
-    return firebase.firestore();
-};
-const currentUser = () => {
-    return firebase.auth().currentUser
-};
-
-const addPostToCloudFirestore = (inputComment, idUser, statusComment) => {
-    const f = new Date();
-    let fecha =  f.getDate() + "-" + (f.getMonth() + 1) + "-" + f.getFullYear();
-    dataBaseCloudFirestore().collection('posts').add({
-        hours: f.getHours() +":"+f.getMinutes(),
-        today: fecha,
-        content: inputComment,
-        userId: idUser,
-        state: statusComment,
-        likes: 0,
-    }).then((docRef) => {
-        console.log(docRef);
-        console.log("Document written with ID: ", docRef.id);
-    })
-        .catch((error) => {
-            console.error("Error adding document: ", error);
-        });
-}
-const deletePostInCloudFireStore = (idPost, idUserOfPost) => {
-
-    const uidOfCurrentUser = currentUser().uid; // id del usuario logueado actual 
-    console.log(uidOfCurrentUser); // id del usuario logueado actual 
-    console.log(idUserOfPost); // id del usuario  dentro del objeto post
-    console.log(idPost); // id del post
-    if (uidOfCurrentUser === idUserOfPost) {
-        dataBaseCloudFirestore().collection("posts").doc(idPost).delete().then(() => {
-            console.log("Document successfully deleted!");
-        }).catch((error) => {
-            console.error("Error removing document: ", error);
-        });
-    } else {
-        alert("You can not delete a comment which was not published by you");
-    }
-};
-const orderByTime=()=>{
-    return dataBaseCloudFirestore().collection('posts').orderBy("hours")
-}
 /*
 
 const upLoadImageToFirestore = (file, callback) => {
@@ -96,16 +28,3 @@ const upLoadImageToFirestore = (date, image) => {
    
 };*/
 
-export {
-    signUp,
-    signIn,
-    signInWithGoogle,
-    signInWithFacebook,
-    signOut,
-    dataBaseCloudFirestore,
-    currentUser,
-    addPostToCloudFirestore,
-    deletePostInCloudFireStore,
-    orderByTime
-    // upLoadImageToFirestore,
-};
