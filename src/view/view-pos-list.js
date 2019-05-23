cee604dc8c86d1be3613f3212c236de90d0e212c
marjo1994 <m_rhl@live.com>
2019-05-22 23:24:15 -0500

    cambios

--- src/view/view-pos-list.js
+++ src/view/view-pos-list.js
@@ -6,36 +6,35 @@ export default (doc, getUser, post, idUserAuth) => {
   const article = document.createElement('article');
   const li = `
     <article id = 'content-post' class= 'flex-container  margin-top border center'> 
-    <div class = 'btn-post-edit-del'>
-    <img class ='img-perfil-post' src='./image/editar.png' alt ='boton de editar' id='btn-edit-${doc.id}'>
-    <img class ='img-perfil-post' src='./image/boton-cancelar.png' alt ='boton para eliminar' id='btn-delete-${doc.id}'>
-    </div>    
-      <header class='header-post'>       
-      <img id='photo-post-user' src='${getUser.data().photo}' alt='feminismo' class='img-perfil-post'>                
-      <label id='name-user-post' class=''>${getUser.data().name}</label> 
-      <label id='fecha-post' class='center color-fecha'>${post.fechaPost}-Hora:⏲ ${post.horaPost}</label>            
-      </header>
-       
-      <select name="" id="estado-post-view-Post-${doc.id}" class = 'size-state-post btn-post-create'>
-      <option  value="${post.state}" class='color-fecha' select>👥${post.state}</option>
-      <option  value="publico">👥Público</option>
-      <option  value="privado">🔐Privado</option>
-      </select>
-      <section class='content-post'>     
-      <img id='image-post-view' src='${post.image}' alt="imagen-post" class='img-post-prev'> 
-      <textarea id = 'description-${doc.id}' class="textarea center">${post.description}</textarea>      
-      </section>
-      <footer class = 'margin-footer center'>
-      <div class = 'style-color-header style-content-post-img'>
-      <button id ='btn-like-${doc.id}' class='btn-post-create'>${post.likes}👍</button>
-      <button id ='btn-love' class='btn-post-create'>❤️Me encanta</button>
-      <button id ='btn-coment-${doc.id}' class='btn-post-create'>Comentar</button> 
-      <div id = 'comment-form'></div>      
-      </div >       
-      </footer>
-      <div id = 'cont-coment-${doc.id}'></div>
-      <div id = 'comment-form-list-${doc.id}'>        
-      </div>       
+      <div class = 'btn-post-edit-del'>
+        <img id='photo-post-user' src='${getUser.data().photo}' alt='feminismo' class='img-perfil-post'>                
+        <label id='name-user-post' class=''>${getUser.data().name}</label> 
+        <label id='fecha-post' class='center'>${post.fechaPost}-Hora:${post.horaPost}</label>  
+        <img class ='img-perfil-post' src='./image/boton-cancelar.png' alt ='boton para eliminar' id='btn-delete-${doc.id}'>
+      </div>    
+        <header class='header-post'>       
+                    
+        </header>
+        
+        <section class='content-post'>     
+          <img id='image-post-view' src='${post.image}' alt="imagen-post" class='img-post-prev'> 
+          <textarea id = 'description-${doc.id}' class="textarea center">${post.description}</textarea>      
+        </section>
+        <footer class = 'margin-footer center'>
+          <div class = 'style-color-header style-content-post-img'>
+            <button id ='btn-like-${doc.id}' class='btn-post-create'>${post.likes}👍</button>
+            <img class ='img-perfil-post' src='./image/editar_lapiz.png' alt ='boton de editar' id='btn-edit-${doc.id}'>
+            <button id ='btn-coment-${doc.id}' class='btn-post-create'>Comentar</button>
+            <select name="" id="estado-post-view-Post-${doc.id}" class = 'size-state-post btn-post-create'>
+              <option  value="${post.state}"select>👥${post.state}</option>
+              <option  value="publico">👥Público</option>
+              <option  value="privado">🔐Privado</option>
+            </select>
+            <div id = 'comment-form'></div>      
+          </div >       
+        </footer>
+        <div id = 'cont-coment-${doc.id}'></div>
+        <div id = 'comment-form-list-${doc.id}'></div>       
     </article>  
         `
   article.innerHTML = li;
